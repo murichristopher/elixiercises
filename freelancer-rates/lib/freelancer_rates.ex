24f0cf94869eb2535fb7fc747b7bec5c1f163a31
murichristopher <murichristopher@outlook.com>
2024-03-01 01:51:27 -0300

    refactor(freelancer_rate): introduce `then` function

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -1,27 +1,24 @@
 defmodule FreelancerRates do
-  @hours_per_workday 8
+  @hours_per_workday 8.0
   @month_billable_days 22
 
-  def daily_rate(hourly_rate), do: @hours_per_workday * (hourly_rate / 1)
+  def daily_rate(hourly_rate), do: @hours_per_workday * hourly_rate
 
   def apply_discount(before_discount, discount),
     do: before_discount - before_discount * (discount / 100)
 
   def monthly_rate(hourly_rate, discount) do
     hourly_rate
+    |> daily_rate()
     |> apply_discount(discount)
-    |> monthly_rate
-    |> ceil
+    |> then(&(@month_billable_days * &1))
+    |> ceil()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
     daily_rate(hourly_rate)
     |> apply_discount(discount)
-    |> days_in_budget(budget)
+    |> then(&(budget / &1))
     |> Float.floor(1)
   end
-
-  defp monthly_rate(hourly_rate), do: @hours_per_workday * @month_billable_days * hourly_rate
-
-  defp days_in_budget(rate, budget), do: budget / rate
 end
