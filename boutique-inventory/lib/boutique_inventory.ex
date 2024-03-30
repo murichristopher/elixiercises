13364e6373f4e17659606a5d36491117fece788b
murichristopher <murichristopher@outlook.com>
2024-03-30 17:21:02 -0300

    feat(boutique_inventory): complete exercise

---
+++ boutique-inventory/lib/boutique_inventory.ex
@@ -0,0 +1,22 @@
+defmodule BoutiqueInventory do
+  def sort_by_price(inventory), do: Enum.sort_by(inventory, & &1.price)
+
+  def with_missing_price(inventory), do: Enum.reject(inventory, & &1.price)
+
+  def update_names(inventory, old_word, new_word) do
+    Enum.map(inventory, fn x ->
+      %{x | name: String.replace(x.name, old_word, new_word)}
+    end)
+  end
+
+  def increase_quantity(item, count) do
+    Map.put(
+      item,
+      :quantity_by_size,
+      Enum.into(item.quantity_by_size, %{}, fn {k, v} -> {k, v + count} end)
+    )
+  end
+
+  def total_quantity(item),
+    do: Enum.reduce(item.quantity_by_size, 0, fn {_, v}, acc -> acc + v end)
+end
