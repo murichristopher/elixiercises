2386bcb7ff2c986f51b13ad9ed56ab5fe6282d34
murichristopher <murichristopher@outlook.com>
2024-11-04 01:43:10 -0300

    refactor(library_fees): refactor exercise

--- library-fees/lib/library_fees.ex
+++ library-fees/lib/library_fees.ex
@@ -5,7 +5,7 @@ defmodule LibraryFees do
     end
   end
 
-  def before_noon?(%NaiveDateTime{} = datetime) do
+  def before_noon?(datetime) do
     datetime.hour < 12
   end
 
@@ -23,52 +23,30 @@ defmodule LibraryFees do
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    difference =
-      NaiveDateTime.to_date(actual_return_datetime)
-      |> Date.diff(planned_return_date)
-
-    if difference < 1 do
-      0
-    else
-      difference
-    end
+    actual_return_datetime
+    |> Date.diff(planned_return_date)
+    |> max(0)
   end
 
   def monday?(datetime) do
-    day_of_week =
-      datetime
-      |> NaiveDateTime.to_date()
-      |> Date.day_of_week()
-
-    day_of_week == 1
+    Date.day_of_week(datetime) == 1
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    {:ok, checkout_date, _} = DateTime.from_iso8601(checkout)
-    {:ok, return_date, _} = DateTime.from_iso8601(return)
+    checkout_date = datetime_from_string(checkout)
+    return_date = datetime_from_string(return)
 
-    checkout_naive_date = DateTime.to_naive(checkout_date)
+    difference =
+      checkout_date
+      |> return_date()
+      |> days_late(return_date)
 
-    difference = days_late(checkout_naive_date, return_date)
-    return_date_naive = return_date |> DateTime.to_naive()
-    expected_return_date = return_date(checkout_naive_date)
+    fee = difference * rate
 
-    difference = Date.diff(return_date_naive, expected_return_date)
-
-    if difference <= 0 do
-      0
-    else
-      apply_monday_discount(difference * rate, return_date)
-    end
+    apply_monday_discount(fee, return_date)
   end
 
   defp apply_monday_discount(fee, return_date) do
-    case monday?(return_date) do
-      true ->
-        trunc(fee * 0.5)
-
-      _ ->
-        fee
-    end
+    if monday?(return_date), do: trunc(fee * 0.5), else: fee
   end
 end
