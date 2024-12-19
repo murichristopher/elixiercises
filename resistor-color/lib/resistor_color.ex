fa69915e788757a5632ef7dc4f5cc050a1f10854
murichristopher <murichristopher@outlook.com>
2024-12-19 14:00:57 -0300

    feat(resistor-color): complete exercise

---
+++ resistor-color/lib/resistor_color.ex
@@ -0,0 +1,23 @@
+defmodule ResistorColor do
+  @doc """
+  Return the value of a color band
+  """
+
+  @color_bands %{
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }
+
+  @spec code(atom) :: integer()
+  def code(color) do
+    @color_bands[color]
+  end
+end
