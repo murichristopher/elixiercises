cb0251d9a08e312c3ae90b153d7d480ac0619f2a
murichristopher <murichristopher@outlook.com>
2024-12-30 19:53:03 -0300

    feat(darts): complete exercise

---
+++ darts/lib/darts.ex
@@ -0,0 +1,18 @@
+defmodule Darts do
+  @type position :: {number, number}
+
+  @doc """
+  Calculate the score of a single dart hitting a target
+  """
+  @spec score(position) :: integer
+  def score({x, y}) do
+    radius = :math.sqrt(x * x + y * y)
+
+    cond do
+      radius <= 1 -> 10
+      radius <= 5 -> 5
+      radius <= 10 -> 1
+      true -> 0
+    end
+  end
+end
