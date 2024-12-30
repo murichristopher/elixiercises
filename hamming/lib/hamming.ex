b7c95d81260db0202e1125a5cf08d8d5a774efbb
murichristopher <murichristopher@outlook.com>
2024-12-30 19:53:03 -0300

    feat(hamming): complete exercise

---
+++ hamming/lib/hamming.ex
@@ -0,0 +1,24 @@
+defmodule Hamming do
+  @doc """
+  Returns number of differences between two strands of DNA, known as the Hamming Distance.
+
+  ## Examples
+
+  iex> Hamming.hamming_distance(~c"AAGTCATA", ~c"TAGCGATC")
+  {:ok, 4}
+  """
+  @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
+
+  def hamming_distance(strand1, strand2) when length(strand1) != length(strand2) do
+    {:error, "strands must be of equal length"}
+  end
+
+  def hamming_distance(strand1, strand2) do
+    result =
+      strand1
+      |> Enum.zip(strand2)
+      |> Enum.count(fn {c1, c2} -> c1 != c2 end)
+
+    {:ok, result}
+  end
+end
